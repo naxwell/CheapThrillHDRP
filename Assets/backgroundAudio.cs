91d33fbb17c382a6fe31fd2d99fefea084b6eece
naxwell <maxwell.lander@gmail.com>
2020-08-06 16:07:40 -0400

    added flashlight and substance

---
+++ Assets/backgroundAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundAudio : MonoBehaviour
+{
+
+    public AudioClip[] backgrounds;
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            playRandomBackground();
+        }
+    }
+
+    public void playRandomBackground()
+    {
+        int clip = Random.Range(0, backgrounds.Length);
+        source.clip = backgrounds[clip];
+        source.Play();
+    }
+}
