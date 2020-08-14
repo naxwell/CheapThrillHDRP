7cde084bc381ab354ca026723a38c42f2afa6780
naxwell <maxwell.lander@gmail.com>
2020-08-14 16:53:26 -0400

    follow meatcube
    post processing effects

---
+++ Assets/followNotLooked.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followNotLooked : MonoBehaviour
+{
+
+    public Transform[] Players;
+    public float speed = 1f;
+    public bool move;
+
+    Renderer m_Renderer;
+    Transform target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        target = Players[Random.Range(0, Players.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_Renderer.isVisible)
+        {
+            move = true;
+        }
+        else
+        {
+            move = false;
+        }
+        if (move)
+        {
+            transform.LookAt(target);
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+    }
+}
