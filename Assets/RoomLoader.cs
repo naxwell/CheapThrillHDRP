157d84b5bb95e420af0bea8aebe07de4452d85c3
naxwell <maxwell.lander@gmail.com>
2020-08-16 18:38:36 -0400

    join room scene
    select room input

---
+++ Assets/RoomLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Normal.Realtime;
+
+public class RoomLoader : MonoBehaviour
+{
+
+    public string roomName;
+    private Realtime _realtime;
+    public Text _text;
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _realtime = GetComponent<Realtime>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roomName = _text.text;
+    }
+
+    public void loadRoom()
+    {
+        _realtime.Disconnect();
+        _realtime.Connect(roomName);
+        //SceneManager.LoadScene(1);
+    }
+}
