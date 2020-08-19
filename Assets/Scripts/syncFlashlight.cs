2514da5a628dece4429ffd5d294ee1ff64f649a9
naxwell <maxwell.lander@gmail.com>
2020-08-19 15:05:36 -0400

    flashlight sync

---
+++ Assets/Scripts/syncFlashlight.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class syncFlashlight : RealtimeComponent
+{
+
+    private Light _spotlight;
+    private flashlightModel _model;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _spotlight = GetComponent<Light>();
+    }
+
+    private flashlightModel model
+    {
+        set
+        {
+
+            if (_model != null)
+            {
+                _model.lightPowerDidChange -= LightPowerDidChange;
+
+            }
+            _model = value;
+
+            if (_model != null)
+            {
+                UpdateFlashlightPower();
+
+                _model.lightPowerDidChange += LightPowerDidChange;
+            }
+        }
+    }
+
+    private void LightPowerDidChange(flashlightModel model, float value)
+    {
+        UpdateFlashlightPower();
+    }
+
+    private void UpdateFlashlightPower()
+    {
+        _spotlight.intensity = _model.lightPower;
+    }
+
+    public void SetPower(float power)
+    {
+        _model.lightPower = power;
+    }
+
+
+}
---
+++ Assets/flashlightTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlightTest : MonoBehaviour
+{
+    [SerializeField]
+    private float _power;
+    private float _prevPower;
+
+    private syncFlashlight _syncFlashlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _syncFlashlight = GetComponent<syncFlashlight>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_power != _prevPower)
+        {
+            _syncFlashlight.SetPower(_power);
+            _prevPower = _power;
+        }
+    }
+}
