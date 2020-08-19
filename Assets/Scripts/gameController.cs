ece74ac1202041b3c1b2e52a5d1243807b4d5f9a
naxwell <maxwell.lander@gmail.com>
2020-08-19 13:23:25 -0400

    power up sync
    script re arrange

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class gameController : MonoBehaviour
+{
+
+    [Header("Lurker Shit")]
+    public GameObject _lurker;
+    public bool _hasLuker = false;
+    private RealtimeTransform _lurkerRealtime;
+    private Lurker _lurkScript;
+
+    [Header("Direction Light Control")]
+
+    public Light _lightSource;
+    private syncLightFlicker _flickerScript;
+    public bool _hasLightingControl = false;
+
+    [Header("Flashlight Control")]
+    public Light Flashlight;
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        _lurker = GameObject.Find("Lurker");
+        _lurkerRealtime = _lurker.GetComponent<RealtimeTransform>();
+        _lurkScript = _lurker.GetComponent<Lurker>();
+        _flickerScript = _lightSource.GetComponent<syncLightFlicker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3") && _hasLuker)
+        {
+            _lurkScript.startLurk();
+
+        }
+
+        if (Input.GetButtonDown("Fire2") && _hasLightingControl)
+        {
+            if (_lightSource.intensity == 75)
+            {
+                StartCoroutine(_flickerScript.TurnOffLight());
+
+            }
+            else if (_lightSource.intensity == 0)
+            {
+                StartCoroutine(_flickerScript.TurnOnLight());
+
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/powerUp.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Normal.Realtime;
+
+public class powerUp : MonoBehaviour
+{
+    [Header("Available Power Up")]
+    public bool lurker;
+    public bool lightingControl;
+    public bool flashlightControl;
+    public bool follower;
+    public bool scream;
+
+    private string[] powerUps = { "lurker", "lightingControl", "flashlightControl", "follower", "scream" };
+    public string availPowerUp;
+
+    public Canvas powerUpCanvas;
+    public Text powerUpText;
+
+    [Header("Location Gen Stuff")]
+    public Collider m_Collider;
+    Vector3 m_Center;
+    Vector3 m_Size, m_Min, m_Max;
+
+
+    private RealtimeView _realtimeView;
+    private RealtimeTransform _realtimeTrans;
+
+
+
+
+
+
+    public gameController _gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        availPowerUp = powerUps[Random.Range(0, powerUps.Length)];
+        powerUpText.text = "Powerup Available Here: " + availPowerUp;
+
+        _realtimeTrans = GetComponent<RealtimeTransform>();
+        _realtimeView = GetComponent<RealtimeView>();
+
+        //find min and max bound of floor collider for power up location generation 
+        m_Min = m_Collider.bounds.min;
+        m_Max = m_Collider.bounds.max;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+
+
+        _realtimeTrans.RequestOwnership();
+        _realtimeView.RequestOwnership();
+
+        if (availPowerUp == "lightingControl")
+        {
+            _gameController._hasLightingControl = true;
+
+
+        }
+
+        if (availPowerUp == "lurker")
+        {
+            _gameController._hasLuker = true;
+        }
+
+        Vector3 newLoc = new Vector3(Random.Range(m_Min.x, m_Max.x), 0, Random.Range(m_Min.z, m_Max.z));
+        transform.position = newLoc;
+        availPowerUp = powerUps[Random.Range(0, powerUps.Length)];
+        powerUpText.text = "Powerup Available Here: " + availPowerUp;
+
+
+
+    }
+
+    void OnTriggerExit()
+    {
+        _realtimeTrans.ClearOwnership();
+        _realtimeView.ClearOwnership();
+    }
+}
